377912e1713c4d8357f51ce0bd32df4b27639078
Philip Jensen <me@iensenfirppu.dk>
2015-12-03 22:09:17 +0100

    Attempt #2 at Travis CI integration

---
+++ tests/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+include_once('../src/classes/Example.php');
+
+class ExampleTest extends PHPUnit_Framework_TestCase
+{
+    public function testMakeSureTestsAreRun()
+    {
+		$this->assertTrue(true);
+    }
+    
+    public function testExampleReturnsTrueAlwaysReturnsTrue()
+    {
+		$this->assertEquals(Example::ReturnsTrue(), true);
+    }
+    
+    public function testExampleReturnsFalseAlwaysReturnsFalse()
+    {
+        $this->assertEquals(Example::ReturnsFalse(), false);
+    }
+}
+?>
